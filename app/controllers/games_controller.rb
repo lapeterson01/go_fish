4c3546f94c246ceb5eef5c634deb7fd873f193d2
Logan Peterson <lapeterson01@gmail.com>
2019-12-05 17:34:07 +0300

    Set up basic view rendering

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,3 @@
+class GamesController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :games, only: %i[show]
+  root 'games#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
